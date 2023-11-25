c51add150fc0b120ab9971e60efdd0e84aa24984
Teague <teaguehannam@gmail.com>
2023-11-25 07:40:01 -0700

    fix Trim

--- netlify/functions/img/main.go
+++ netlify/functions/img/main.go
@@ -15,7 +15,7 @@ import (
 const baseURL = "https://dchqsxvlazultvsphmau.supabase.co/storage/v1/object/public/"
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	imagePath := strings.TrimPrefix(request.Path, "/.netlify/functions/img/")
+	imagePath := strings.TrimPrefix(request.Path, "/cdn/")
 	imageURL := baseURL + imagePath
 
 	// Fetch the image
